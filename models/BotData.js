9e00a1683770fd4b98f52a6e9c0fd6dab3a918e6
aras737 <erdemiraraseymen936@gmail.com>
2026-05-17 07:46:39 +0300

    Delete models/BotData.js

--- models/BotData.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const botDataSchema = new mongoose.Schema({
-  key: { type: String, required: true, unique: true },
-  value: mongoose.Schema.Types.Mixed,
-  updatedAt: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('BotData', botDataSchema);
